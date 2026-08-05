34fa021be98554cad8621e8bf7b3eb7db219a890
Jorge Casas <j.casas.lopez.26@gmail.com>
2026-08-05 11:28:08 +0200

    feat: add public Swagger paths as constants

---
+++ src/main/java/dev/jcasaslopez/classroom/shared/utility/PublicSwaggerPaths.java
@@ -0,0 +1,8 @@
+package dev.jcasaslopez.classroom.shared.utility;
+
+public class PublicSwaggerPaths {
+	
+	public static final String SWAGGER_UI =  "/swagger-ui";
+	public static final String API_DOCS =  "/v3/api-docs";
+
+}
